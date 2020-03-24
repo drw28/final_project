90ca1581859e944ce013db6df8c50d9ec20af610
Doug Wicks <drw28@bath.ac.uk>
2020-03-24 18:30:36 +0000

    merge modifications

--- project_data/scripts/hmedip/merge_test.sh
+++ project_data/scripts/hmedip/merge_test.sh
@@ -2,11 +2,11 @@
 
 DIR=/Volumes/Recovered_Mac/project
 analysis="${DIR}"/project_data/analysis/hmeDIP-seq
-raw_data="${analysis}"/raw-test
+raw_data="${analysis}"/raw-data
 
 #Merge "_p2" file names using 'for' loop
 
-if [ -a "${raw_data}"/1Bound_p2.927.s_1.r_1.fq.gz ]; then
+if [ -a "${raw_data}"/1Bound_p2.927.s_1.r_1.fq.gz ] ; then
   cat "${raw_data}"/1Bound.927.s_1.r_1.fq.gz "${raw_data}"/1Bound_p2.927.s_1.r_1.fq.gz \
   > "${raw_data}"/1Bound.927.s_1m.r_1.fq.gz
   rm 1Bound.927.s_1.r_1.fq.gz 1Bound_p2.927.s_1.r_1.fq.gz
@@ -15,7 +15,7 @@ else
   echo "1Bound already merged"
 fi
 
-if [ -a  "${raw_data}"/6Bound_p2.927.s_2.r_1.fq.gz ]; then
+if [ -a  "${raw_data}"/6Bound_p2.927.s_2.r_1.fq.gz ] ; then
   cat "${raw_data}"/6Bound.927.s_2.r_1.fq.gz "${raw_data}"/6Bound_p2.927.s_2.r_1.fq.gz \
   > "${raw_data}"/6Bound.927.s_2m.r_1.fq.gz
   rm 6Bound.927.s_2.r_1.fq.gz 6Bound_p2.927.s_2.r_1.fq.gz
